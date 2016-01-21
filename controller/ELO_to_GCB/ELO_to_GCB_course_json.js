8ac5f96ea02aa9cea863aa5a23c58183f625be6f
howard <howard55662004@yahoo.com>
2016-01-22 04:51:32 +0800

    fixes the bugs in ELO_to_GCB_course_json.js

--- controller/ELO_to_GCB/ELO_to_GCB_course_json.js
+++ controller/ELO_to_GCB/ELO_to_GCB_course_json.js
@@ -32,7 +32,7 @@ function write_unit_course_json(myunit_name, myunit_id){
 	"\n\t  \"labels\": \"\",\n\t  \"manual_progress\": false,\n\t  \"now_available\": false," + 
 	"\n\t  \"post_assessment\": null,\n\t  \"pre_assessment\": null,\n\t  \"properties\": {}," + 
 	"\n\t  \"release_date\": \"\",\n\t  \"show_contents_on_one_page\": false,\n\t  \"shown_when_unavailable\":" + 
-	" false,\n\t  \"title\": " + myunit_name + ",\n\t  \"type\": \"U\",\n\t  \"unit_footer\": \"\"," + 
+	" false,\n\t  \"title\": \"" + myunit_name + "\",\n\t  \"type\": \"U\",\n\t  \"unit_footer\": \"\"," + 
 	"\n\t  \"unit_header\": \"\",\n\t  \"unit_id\": " + myunit_id + ",\n\t  \"weight\": 1,\n\t  \"workflow_yaml\": " + 
 	"\"grader: auto\\n\"\n\t},", function(err){
 		if(err) throw err;
@@ -49,6 +49,7 @@ function gcb_course_json(){
 	var gcb_path = file1.path.replace(file1.name, "") + "GCB " + new_file_name.replace(/ /g, "_");
 	var buf2 = new Buffer(1000000);
 	var buf3 = new Buffer(1000000);
+	var unit_id = 1;
 
 	fs.open(gcb_path + "/files/data/course.json", "w", function(err,fd){
 		if(err) throw err;
@@ -78,35 +79,41 @@ function gcb_course_json(){
 						x = xmlDoc.getElementsByTagName("container");
 						y = xmlDoc.getElementsByTagName("content");
 
-						for(var i = 0; i < x.length; i++){
-							var unit_id = i + 1;
-							var unit_name = x[i].getAttribute("display_name");
-							var lesson_id = unit_id + x.length;
-							var html_file_name = y[i].getAttribute("url_name");
-							console.log(unit_id);
-							console.log(unit_name);
-							console.log(lesson_id);
-							console.log(html_file_name);
+						for(var i = 1; i < y.length; i++){
 
-							if(i == x.length - 1){
+							var unit_name = x[unit_id-1].getAttribute("display_name");
+							var lesson_id = i + y.length;
+							var html_file_name = y[i-1].getAttribute("url_name");
 
-								fs.appendFile(gcb_path + "/files/data/course.json", 
-								"\n\t{\n\t  \"activity_listed\": true,\n\t  \"activity_title\": \"\",\n\t  \"auto_index\": true," + 
-								"\n\t  \"duration\": \"\",\n\t  \"has_activity\": false,\n\t  \"lesson_id\": " + lesson_id + 
-								",\n\t  \"manual_progress\": false,\n\t  \"notes\": \"assets/html/" + html_file_name + ".html\"," + 
-								"\n\t  \"now_available\": false,\n\t  \"objectives\": \"\",\n\t  \"properties\": {\n\t    \"" + 
-								"modules.skill_map.skill_list\": []\n\t  },\n\t  \"scored\": false,\n\t  \"title\": \"" + unit_name + 
-								"\",\n\t  \"unit_id\": " + unit_id + ",\n\t  \"video\": \"\"\n\t}\n  ],\n  \"next_id\": " + 
-								(lesson_id + 1) + ",\n  \"units\": [", function(err){
-																	
-									if(err) throw err;
-							  		console.log(' gcb_manifest_content was complete!');
-								})
+							if(y[i-1].getAttribute("tid") == y[i].getAttribute("tid")){
+								write_lesson_course_json(unit_name, lesson_id, unit_id, html_file_name);
 							}
 							else{
 								write_lesson_course_json(unit_name, lesson_id, unit_id, html_file_name);
+								unit_id += 1;
+								console.log(unit_id);
 							}
 						}
+
+						var unit_name = x[x.length-1].getAttribute("display_name");
+						var html_file_name = y[y.length-1].getAttribute("url_name");
+						var lesson_id = 2 * y.length;
+						console.log(html_file_name);
+
+
+						fs.appendFile(gcb_path + "/files/data/course.json", 
+						"\n\t{\n\t  \"activity_listed\": true,\n\t  \"activity_title\": \"\",\n\t  \"auto_index\": true," + 
+						"\n\t  \"duration\": \"\",\n\t  \"has_activity\": false,\n\t  \"lesson_id\": " + lesson_id + 
+						",\n\t  \"manual_progress\": false,\n\t  \"notes\": \"assets/html/" + html_file_name + ".html\"," + 
+						"\n\t  \"now_available\": false,\n\t  \"objectives\": \"\",\n\t  \"properties\": {\n\t    \"" + 
+						"modules.skill_map.skill_list\": []\n\t  },\n\t  \"scored\": false,\n\t  \"title\": \"" + unit_name + 
+						"\",\n\t  \"unit_id\": " + unit_id + ",\n\t  \"video\": \"\"\n\t}\n  ],\n  \"next_id\": " + 
+						(lesson_id + 1) + ",\n  \"units\": [", function(err){
+															
+							if(err) throw err;
+					  		console.log(' course.json about lesson was complete!');
+						})
+
 						setTimeout(function(){
 							for(var j = 0; j < x.length; j++){
 								
@@ -122,7 +129,7 @@ function gcb_course_json(){
 									"\n\t  \"labels\": \"\",\n\t  \"manual_progress\": false,\n\t  \"now_available\": false," + 
 									"\n\t  \"post_assessment\": null,\n\t  \"pre_assessment\": null,\n\t  \"properties\": {}," + 
 									"\n\t  \"release_date\": \"\",\n\t  \"show_contents_on_one_page\": false,\n\t  \"shown_when_unavailable\":" + 
-									" false,\n\t  \"title\": " + unit_name2 + ",\n\t  \"type\": \"U\",\n\t  \"unit_footer\": \"\"," + 
+									" false,\n\t  \"title\": \"" + unit_name2 + "\",\n\t  \"type\": \"U\",\n\t  \"unit_footer\": \"\"," + 
 									"\n\t  \"unit_header\": \"\",\n\t  \"unit_id\": " + unit_id2 + ",\n\t  \"weight\": 1,\n\t  \"workflow_yaml\": " + 
 									"\"grader: auto\\n\"\n\t}\n  ],\n  \"version\": \"1.3\"\n}", function(err){
 										if(err) throw err;
@@ -132,7 +139,7 @@ function gcb_course_json(){
 									write_unit_course_json(unit_name2, unit_id2);
 								}
 							}
-						}, 20)
+						}, 30)
 					}
 				})
 			})
